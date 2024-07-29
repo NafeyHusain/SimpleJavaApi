00c89148754af772cca192ff34078fdccb37a6d7
nafeyah <nafeyahthenx@gmail.com>
2024-07-30 00:18:35 +0530

    first commit

---
+++ src/test/java/com/minikubeapplication/MiniKubeApplication/MiniKubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minikubeapplication.MiniKubeApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniKubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
